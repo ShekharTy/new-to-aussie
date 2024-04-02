312f0fdfd7f689593af0b517bf3d2051cc1d71ca
Mirudhula <mraj0010@student.monash.edu>
2024-04-02 16:53:54 +1100

    swim safety module added

--- src/components/beach-safety.js
+++ src/components/beach-safety.js
@@ -4,17 +4,28 @@ import Header from './header';
 import '../styles/beach-safety.css';
 
 function BeachSafety() {
+
     let navigate = useNavigate();
 
     useEffect(() => {
         document.title = `Beach Safety`;
     }); 
-
+    // Sub-module components
+    function SwimmingSafety() {
+        return (
+            <div>
+                <h2>Swimming Safety</h2>
+                {/* Add swimming safety content */}
+            </div>
+        );
+    }
     return (
         <div>
             <Header/>
             <div className='body'>
                 Getting started
+                {/*Render sub modules */}
+                <SwimmingSafety />
             </div>
         </div>
     );
