f883065392bf2e1ac8d7abff36d1f71cbbe065fe
Zewen GU <zewengu@Zewens-MBP.modem>
2024-04-04 01:54:17 +1100

    Implement beach safety quiz feature

--- src/components/beach-safety.js
+++ src/components/beach-safety.js
@@ -2,12 +2,21 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './header';
 import '../styles/beach-safety.css';
+<<<<<<< HEAD
 
+=======
+import Sidebar from '../components/sidebar';
+import Quiz from './quiz';
+>>>>>>> 5aced8a (Implement beach safety quiz feature)
 
 function BeachSafety() {
     useEffect(() => {
         document.title = `Beach Safety`;
     }, []);
+
+    const [showQuiz, setShowQuiz] = useState(false);
+
+    const toggleQuiz = () => setShowQuiz(!showQuiz);
 
     return (
         <div>
@@ -27,16 +36,26 @@ function BeachSafety() {
                         <li>
                             <Link to="/warning">Learn Beach Warning Signs</Link>
                         </li>
+<<<<<<< HEAD
                         <li>
                             <Link to="/beachquiz">Check your learnings - Quiz </Link>
+=======
+                        {/* Toggle quiz visibility from the BeachSafety page */}
+                        <li>
+                            <button onClick={toggleQuiz}>Take the Quiz</button>
+>>>>>>> 5aced8a (Implement beach safety quiz feature)
                         </li>
                     </ul>
                     
                     {/* Add other main content here */}
+                    {/* Conditionally render the Quiz component */}
+                    {showQuiz && <Quiz />}
                 </div>
             </div>
         </div>
     );
 }
+
+  
 
 export default BeachSafety;
