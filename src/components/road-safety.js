a5be368430b9f54597c44999fcd3f1ae23f98c3d
Shekhar Tyagi <stya0003@student.monash.edu>
2024-04-03 17:46:57 +1100

    Update road safety

--- src/components/road-safety.js
+++ src/components/road-safety.js
@@ -1,24 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './header';
 import Footer from './footer';
 import '../styles/road-safety.css';
 
 function RoadSafety() {
+    const [selectedModule, setSelectedModule] = useState('');
 
     useEffect(() => {
         document.title = `Road Safety`;
-    }); 
+    });
+
+    // Function to update the selected module
+    const handleModuleClick = (module) => {
+        setSelectedModule(module);
+    };
 
     return (
         <div>
-            <Header/>
-            <div className='body'>
-
+            <Header />
+            <div className="road-safety-container">
+                <aside className="sidebar">
+                    <h2>Road Safety and Regulations</h2>
+                    <ul>
+                        <li className={selectedModule === 'Traffic signs' ? 'active' : ''} onClick={() => handleModuleClick('Traffic signs')}>Traffic signs</li>
+                        <li className={selectedModule === 'RoadWorks' ? 'active' : ''} onClick={() => handleModuleClick('RoadWorks')}>RoadWorks</li>
+                        <li className={selectedModule === 'Myth Buster' ? 'active' : ''} onClick={() => handleModuleClick('Myth Buster')}>Myth Buster</li>
+                    </ul>
+                    <h2>Check your learning</h2>
+                    <ul>
+                        <li className={selectedModule === 'Quiz' ? 'active' : ''} onClick={() => handleModuleClick('Quiz')}>Quiz</li>
+                    </ul>
+                </aside>
+                <div className="content">
+                    {selectedModule === 'Traffic signs' && <h1>Traffic Signs Content</h1>}
+                    {selectedModule === 'RoadWorks' && <h1>Roadworks Content</h1>}
+                    {selectedModule === 'Myth Buster' && <h1>Myth Buster</h1>}
+                    {selectedModule === 'Quiz' && <h1>Quiz</h1>}
+                </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 }
-
 
 export default RoadSafety;
