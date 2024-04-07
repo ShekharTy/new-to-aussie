3488965345923d61a541c1692da48951551b86f7
Mirudhula <mraj0010@student.monash.edu>
2024-04-07 17:10:39 +1000

    added pics for scenario

--- src/components/scenario-component.js
+++ src/components/scenario-component.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import '../styles/scenario-component.css';
+import drownImage from '../data/drowning.png';
+import stormImage from '../data/strongwinds.png';
+import ripImage from '../data/ripcurrent.png';
+import sharkImage from '../data/sharkimage.png';
+
 // Beach Safety V2
 function ScenarioComponent() {
   const scenarios = [
     {
       description: 'You are at the beach and notice a swimmer in distress. What do you do?',
+      image: drownImage, // Image for scenario 1
       choices: [
         { id: 1, text: 'Call for help and notify a lifeguard.', feedback: 'Great choice! Lifeguards are trained to handle emergencies.' },
         { id: 2, text: 'Attempt to rescue the swimmer yourself.', feedback: 'Attempting to rescue without proper training can be dangerous. It’s best to call for help.' }
@@ -12,16 +18,26 @@ function ScenarioComponent() {
     },
     {
       description: 'You see Strong Winds Warning Sign. What do you do?',
+      image: stormImage, // Image for scenario 2
       choices: [
         { id: 1, text: 'Understand the symbol and stay away from beach.', feedback: 'Excellent decision! Warnings should be adhered strictly to have a safe and enjoable experience.' },
         { id: 2, text: 'Ignore the signs and do parasailing', feedback: 'Do not ignore the warnings, It should be followed.' }
       ]
     },
     {
-      description: 'You notice someone using inflatable toys or rafts in a non-designated swimming area. What do you do?',
+      description: 'At the beach, you notice signs warning about rip currents. What do you decide about doing next?',
+      image: ripImage,
       choices: [
-        { id: 1, text: 'Inform them about the designated swimming areas and advise them to move there.', feedback: 'Good choice! It’s important to educate beachgoers about safety rules and designated swimming areas.' },
-        { id: 2, text: 'Ignore it and continue with your activities.', feedback: 'Ignoring safety violations can lead to accidents. It’s important to address potential hazards to prevent accidents.' }
+        { id: 1, text: 'You choose not to enter the water, advising your friends to stay close to shore..', feedback: 'Excellent choice! Safety comes first. Avoiding areas with rip currents is the best way to stay safe at the beach.' },
+        { id: 2, text: ' You decide to swim but promise to watch for signs of rip currents, believing you can handle any situation.', feedback: 'Risky move! Even strong swimmers can get caught in rip currents. Always respect warning signs to prevent dangerous situations.' }
+      ]
+    },
+    {
+      description: 'You spot a shark in the water nearby. What precautions should you take, and how you respond to the presence of sharks?',
+      image: sharkImage,
+      choices: [
+        { id: 1, text: 'Exit the water as quickly as possible & inform others.', feedback: 'Wise decision! Leaving the water promptly reduces the risk of encountering the shark. Alerting others helps ensure everyones safety..' },
+        { id: 2, text: ' Stay calm and monitor.', feedback: 'Risky decision! While staying calm is important, remaining in the water near a shark can pose a significant danger. It is best to exit the water and seek safety onshore.' }
       ]
     }
   ];
@@ -40,6 +56,7 @@ function ScenarioComponent() {
         <div key={index}>
           <h2>Scenario {index + 1}</h2>
           <p>{scenario.description}</p>
+          <img src={scenario.image} alt={`Scenario ${index + 1}`} />
           <div className="choices">
             {scenario.choices.map(choice => (
               <button key={choice.id} onClick={() => handleChoiceSelection(index, choice.id)}>
